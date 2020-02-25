add56cf07152ddb9fa75272d9b68c1f0ae027a10
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-25 15:04:01 +0100

    Add validation

--- FirstTests/contact/index.php
+++ FirstTests/contact/index.php
@@ -1,4 +1,7 @@
 <?php
+require 'lib/validators.php';
+
+define('CONTACT_PATH', 'contacts.csv');
 
 // a l 'ancienne
 $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
@@ -9,19 +12,40 @@ $email = $_POST['email'] ?? '';
 $phone = $_POST['phone'] ?? '';
 
 $contacts = [];
+$errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // sauvegarde en csv (comma separated value)
     // firstname,lastname,email,phone
 
+    //Ici on valide les données
+    if (isEmpty($firstname)) {
+        $errors[] = 'firstname is required';
+    }
+    if (isEmpty($lastname)) {
+        $errors[] = 'lastname is required';
+    }
+    if (isEmpty($email)) {
+        $errors[] = 'email is required';
+    }
+    if (isEmpty($phone)) {
+        $errors[] = 'phone is required';
+    }
+
     // "" pour pouvoir interpreter les donnees a l'interieur
-    $row = "$firstname,$lastname,$email,$phone\n";
-    file_put_contents('contacts.csv', $row, FILE_APPEND);
+    if (empty($errors)) {
+        $firstname = strip_tags($firstname);
+        $lastname = strip_tags($lastname);
+        $email = strip_tags($email);
+        $phone = strip_tags($phone);
+        $row = "$firstname,$lastname,$email,$phone\n";
+        file_put_contents(CONTACT_PATH, $row, FILE_APPEND);
+    }
 }
 
 //get the content
 // 'r' === read
-$handle = fopen('contacts.csv', 'r');
+$handle = fopen(CONTACT_PATH, 'r');
 
 if ($handle !== false) {
     while (($data = fgetcsv($handle, 1024)) !== false) {
