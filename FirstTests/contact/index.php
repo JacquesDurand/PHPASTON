55fe61ab230d67a505e40717617e08a61feb4cbf
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-02-25 14:07:51 +0100

    Add contact form

---
+++ FirstTests/contact/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// a l 'ancienne
+$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
+
+//mieux
+$lastname = $_POST['lastname'] ?? '';
+$email = $_POST['email'] ?? '';
+$phone = $_POST['phone'] ?? '';
+
+$contacts = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // sauvegarde en csv (comma separated value)
+    // firstname,lastname,email,phone
+
+    // "" pour pouvoir interpreter les donnees a l'interieur
+    $row = "$firstname,$lastname,$email,$phone\n";
+    file_put_contents('contacts.csv', $row, FILE_APPEND);
+}
+
+//get the content
+// 'r' === read
+$handle = fopen('contacts.csv', 'r');
+
+if ($handle !== false) {
+    while (($data = fgetcsv($handle, 1024)) !== false) {
+        $contacts[] = [
+            'firstname' => $data[0],
+            'lastname' => $data[1],
+            'email' => $data[2],
+            'phone' => $data[3],
+        ];
+    };
+}
+
+include 'page.phtml';
